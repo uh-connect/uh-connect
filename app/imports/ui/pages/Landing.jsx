a020538acd57247a94b80f9542e0672b158bdaf5
lsanderson1 <llsanderson98@gmail.com>
2023-11-14 16:53:53 -1000

    landing page adjustments

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
-import { Col, Container, Image, Row, Navbar } from 'react-bootstrap';
+import { Col, Container, Navbar, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
   <div>
     <Navbar style={{ backgroundColor: 'darkgreen', color: 'white' }} expand="lg">
-      <Container>
+      <Container className="w-100">
         <Col className="text-center pb-3 pt-3">
           <h1>Welcome to UH Connect</h1>
           <h5>Bringing Students and Employers Together</h5>
+          <h3>Sign Up Here if You Are:</h3>
+          <Col>
+            <div className="button-container">
+              <Link to="/">
+                <Button size="lg" variant="light" className="gap-3">Student</Button>
+              </Link>
+              <Link to="/">
+                <Button size="lg" variant="light" className="gap-3">Company</Button>
+              </Link>
+            </div>
+          </Col>
         </Col>
       </Container>
     </Navbar>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-        </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Col>
-
-      </Row>
+    <Container id="landing-picture" fluid className="landing-picture">
+      <div className="text-center">
+        <h1>Our Motto:</h1>
+      </div>
     </Container>
   </div>
 );
